af7a8bad34a9f2c70a18dc84e9504d6f64e3b22c
Alfred <alfredyu0911@gmail.com>
2018-03-18 15:02:47 +0800

    [Add] FreeMind file upload, 20180318 Alfred

---
+++ FreeMind/Computer Organization and Architecture/Data path.mm
@@ -0,0 +1,22 @@
+<map version="1.0.0"><node ID="0750FB4B-9C6C-45D5-A35B-BC7789FDAA64" BACKGROUND_COLOR="#FFFFFF" TEXT="Data path" COLOR="#195468" POSITION="right" STYLE="bubble"><edge COLOR="#1A5469" WIDTH="4" /><font NAME="HelveticaNeue-Light" SIZE="24" /><node ID="D0DF14D9-7FD3-4133-A535-E489FC409F13" TEXT="R-type" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="DC355DCA-076D-41A8-A31A-3727550D2547" BACKGROUND_COLOR="#FFFFFF" TEXT="" COLOR="#435B66" POSITION="right" STYLE="bubble"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="12EC2C99-2249-4851-809F-1E43BE57A93B" TEXT="add, sub, sll, etc." COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="8A593831-B859-42AF-98D5-2E1CDF7FD6F5" TEXT="op(6) | rs(5) | rt(5) | rd(5) | shamt(5) | func(6)" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#1286DE" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+<node ID="1DD2620F-3DDB-496A-BDC9-1ADA7946E52D" TEXT="lw" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="78AF9567-1D15-4206-868E-3D94FFA69A15" BACKGROUND_COLOR="#FFFFFF" TEXT="" COLOR="#435B66" POSITION="right" STYLE="bubble"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="F88E7B53-E26E-4BCA-8A7D-B0AA09FF8A74" TEXT="lw" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="8A3478D2-68B1-43F6-9CFB-2319FBC5A3B7" TEXT="op(6) | rs(5) | rt(5) | imm(16)" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#25BAD1" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+<node ID="06EF6C54-C383-4B96-A6AB-5024791053C4" TEXT="sw" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="96980768-62A8-4053-AC1A-C9C1A1340310" BACKGROUND_COLOR="#FFFFFF" TEXT="" COLOR="#435B66" POSITION="right" STYLE="bubble"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="34477DD6-9495-457A-B475-899B7461EBDC" TEXT="sw" COLOR="#425A65" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="0BBA54A6-B29B-4F91-8D28-0EB620455E5A" TEXT="op(6) | rs(5) | rt(5) | imm(16)" COLOR="#415964" POSITION="right" STYLE="fork"><edge COLOR="#389237" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+<node ID="0B069332-BCDB-4931-9691-C177B211E5D7" TEXT="jump" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="68A8EE69-4855-43DB-A268-02F245071614" BACKGROUND_COLOR="#FFFFFF" TEXT="" COLOR="#435B66" POSITION="left" STYLE="bubble"><edge COLOR="#FCB60D" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="C9F33B0C-746F-4EC9-AC81-41B04E1EF261" TEXT="j" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="C4F32A67-0D12-483C-A3BD-98AB37BF301A" TEXT="op(6) | addr(26)" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#FCB60D" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+<node ID="DF160FD4-44DC-4DE8-BA05-0B067EC38EC6" TEXT="branch" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="FE5E6A03-9AFF-4647-B4F8-7CB28610FBF0" BACKGROUND_COLOR="#FFFFFF" TEXT="" COLOR="#435B66" POSITION="left" STYLE="bubble"><edge COLOR="#922AA4" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /><node ID="791493F0-6EEF-469B-94F8-8CE55B690DA9" TEXT="beq, bne, etc." COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+<node ID="4B05CEC7-6A3E-45A7-A8B0-C0696B635EC2" TEXT="op(6) | rs(5) | rt(5) | imm(16)" COLOR="#425A65" POSITION="left" STYLE="fork"><edge COLOR="#922AA4" WIDTH="6" /><font NAME="HelveticaNeue" SIZE="18" /></node>
+</node>
+</node>
+</node>
+</map>
